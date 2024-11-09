74dc6394e220e0d8e1bed8cd4887897e784af3de
Halil Onur Arslanturk <halilonur2009@hotmail.com>
2024-11-09 18:18:54 +0300

    Created Models - added TaskItem model - added User model

---
+++ backend/TaskManagementApp/TaskManagementApp/Models/TaskItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagementApp.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int TaskItemId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        // Foreign Key
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
---
+++ backend/TaskManagementApp/TaskManagementApp/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public ICollection<TaskItem> TaskItems { get; set; }
+    }
+}
